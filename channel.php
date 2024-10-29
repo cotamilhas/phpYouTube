5dd0f5d3fa78097f676fd80f775a6b4d0e9daa2f
Henrique <104533111+cotamilhas@users.noreply.github.com>
2024-10-29 20:19:56 +0000

    Add files via upload

--- channel.php
+++ channel.php
@@ -2,7 +2,14 @@
 $channelhandle = $_GET['id'];
 $apikey = 'YOUR_API_KEY';
 $channelId = getChannelId($channelhandle, $apikey);
+
+if($channelId === "Channel not found."){
+    echo $channelId;
+    exit;
+}
+
 $channelSnippet = channelSnippet($channelhandle, $apikey);
+$channelStatistics = channelStatistics($channelhandle, $apikey);
 
 // getting json
 function getJSONContent($url) {
@@ -14,19 +21,21 @@ function getJSONContent($url) {
 
 // channel id
 function getChannelId($channelhandle, $apikey){
-    $url = 'https://www.googleapis.com/youtube/v3/channels?&forHandle=' . $channelhandle . '&key=' . $apikey;
+    $url = "https://www.googleapis.com/youtube/v3/channels?&forHandle=$channelhandle&key=$apikey";
     $json = getJSONContent($url);
 
     return $json['items'][0]['id'] ?? "Channel not found.";
 }
+
 // channel snippet aka getting channel's avatar, about description and username
 function channelSnippet($channelhandle, $apikey){
-    $url = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&forHandle=' . $channelhandle . '&key=' . $apikey;
+    $url = "https://www.googleapis.com/youtube/v3/channels?part=snippet&forHandle=$channelhandle&key=$apikey";
     $json = getJSONContent($url);
 
     $channelUsername = $json['items'][0]['snippet']['title'];
     $channelDescription = $json['items'][0]['snippet']['description'];
-    $channelAvatar = '<img src="' . $json['items'][0]['snippet']['thumbnails']['medium']['url'] . '">';
+    // medium because I think it is enough, change to high or default if you want...
+    $channelAvatar = '<img src="' . $json['items'][0]['snippet']['thumbnails']['medium']['url'] . '" alt="Channel Icon">'; 
 
     return [
         'username' => $channelUsername,
@@ -35,9 +44,36 @@ function channelSnippet($channelhandle, $apikey){
     ];
 }
 
+// channel statistics aka getting channel's total views, number of subscriber and number of total videos.
+function channelStatistics($channelhandle, $apikey){
+    $url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&forHandle=$channelhandle&key=$apikey";
+    $json = getJSONContent($url);
+
+    $totalViews = $json['items'][0]['statistics']['viewCount'];
+    $hiddenSubCount = $json['items'][0]['statistics']['hiddenSubscriberCount'];
+    // this is already impossible to do, hiding youtube subscribers, but if one day youtube come back with this idea...
+    if ($hiddenSubCount == false)
+        $totalSubscribers = $json['items'][0]['statistics']['subscriberCount'];
+    $totalVideos = $json['items'][0]['statistics']['videoCount'];
+
+    return [
+        'totalViews' => $totalViews,
+        'subscribers' => $totalSubscribers,
+        'totalVideos' => $totalVideos
+    ];
+}
+
+// channel id
 echo "ID: " . $channelId;
+
+// snippet
 echo "<h1>" . $channelSnippet['username'] . "</h1>";
 echo "<p>" . $channelSnippet['description'] . "</p>";
 echo $channelSnippet['avatar'];
+
+// statistics
+echo "<p>Total View: " . $channelStatistics['totalViews'] . "</p>";
+echo "<p>Subscribers: " . $channelStatistics['subscribers'] . "</p>";
+echo "<p> Total Videos: " . $channelStatistics['totalVideos']. "</p>";
 
 ?>
--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:title" content="phpYouTube" />
+    <meta property="og:title" content="YT Project" />
     <meta property="og:description" content="See YouTube channels info">
-    <meta property="og:image" content="" />
+    <meta name="author" content="cotamilhas">
+    <meta property="og:image" content="https://papbyhenrique.pt/yt/img/logo.png" />
     <meta http-equiv="refresh" content="300">
-    <link rel="stylesheet" href="css/indexstyle.css">
+    <link rel="stylesheet" href="css/searchstyle.css">
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <title>YT Project</title>
 </head>
@@ -18,7 +19,7 @@
         <input id="ch" type="text" autocomplete="off" spellcheck="false" placeholder="Ex: sinkMNR">
         <button onclick="gotoSearch()">Search</button>
     </div>
-
+    <!-- NOT THE BEST SEARCH METHOD! but it does the same, I will change this in the future using PHP -->
     <script>
         document.getElementById('ch').addEventListener('keypress', function(event) {
             if (event.key === 'Enter') {
