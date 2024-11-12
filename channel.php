67d432ea2cae9245f0eed658ec43bc6bf40d829b
Henrique <hamilheiro@gmail.com>
2024-11-12 14:12:59 +0000

    Fixed meta image

--- channel.php
+++ channel.php
@@ -25,7 +25,7 @@ addChannelContent($config, $channelId, $channelSnippet, $channelStatistics, $cha
     <meta property="og:title" content="<?php echo $channelSnippet['username']; ?>" />
     <meta property="og:description" content="<?php echo "See more about {$channelSnippet['username']} YouTube channel!"; ?>">
     <meta name="author" content="cotamilhas">
-    <meta property="og:image" content="./channel/<?php echo $channelId; ?>/avatar.png" />
+    <meta property="og:image" content="<?php echo $channelSnippet['avatarUrl']; ?>" />
     <link rel="icon" type="image/png" href="./channel/<?php echo $channelId; ?>/avatar.png"/>
     <link rel="stylesheet" href="./css/channelstyle.css">
     <title>phpYouTube</title>
